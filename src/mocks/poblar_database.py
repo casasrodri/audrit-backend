4490496e75b8f0fc871465ead77e8e88fbb2f817
Rodri Casas <cr.rodrigocasas@gmail.com>
2024-04-09 23:52:51 -0300

    eliminacion sigla_extendida

--- src/mocks/poblar_database.py
+++ src/mocks/poblar_database.py
@@ -5,13 +5,13 @@ cur = con.cursor()
 
 cur.execute(
     """
-    INSERT INTO ciclos (sigla, sigla_extendida, nombre, descripcion, padre_id) VALUES
-        ('DEP', 'DEP', 'Depósitos', 'Cuentas a la vista', null),
-        ('CA', 'DEP-CA', 'Caja de ahorro', 'No dan interés', 1),
-        ('CC', 'DEP-CC', 'Cuenta corriente', 'Podes girar en descubierto', 1),
-        ('PRES', 'PRES', 'Préstamos', 'Plata prestada', null),
-        ('CONS', 'PRES-CONS', 'Consumo y vivienda', 'Para la gente del pueblo', 4),
-        ('PLD', 'PLD', 'Prevención de Lavado de Dinero', 'No vamos a permitir el lavado', null)
+    INSERT INTO ciclos (sigla, nombre, descripcion, padre_id) VALUES
+        ('DEP', 'Depósitos', 'Cuentas a la vista', null),
+        ('CA', 'Caja de ahorro', 'No dan interés', 1),
+        ('CC', 'Cuenta corriente', 'Podes girar en descubierto', 1),
+        ('PRES', 'Préstamos', 'Plata prestada', null),
+        ('CONS', 'Consumo y vivienda', 'Para la gente del pueblo', 4),
+        ('PLD', 'Prevención de Lavado de Dinero', 'No vamos a permitir el lavado', null)
     """
 )
 
