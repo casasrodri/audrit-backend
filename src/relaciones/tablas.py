6af7230876e8ceacab09790f1dd00a7ef180fe93
Rodri Casas <cr.rodrigocasas@gmail.com>
2024-05-08 00:31:29 -0300

    recuperado tabla riesgos_documentos

--- src/relaciones/tablas.py
+++ src/relaciones/tablas.py
@@ -13,3 +13,16 @@ riesgos_objetivos_control = Table(
     ),
     extend_existing=True,
 )
+
+riesgos_documentos = Table(
+    "riesgos_documentos",
+    BaseSchema.metadata,
+    Column("riesgo_id", Integer, ForeignKey("riesgos.id"), primary_key=True),
+    Column(
+        "documentos_id",
+        Integer,
+        ForeignKey("documentos.id"),
+        primary_key=True,
+    ),
+    extend_existing=True,
+)
