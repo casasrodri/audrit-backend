bddc11a2a22a1032103e47a5e65e73c2530affc4
Rodri Casas <cr.rodrigocasas@gmail.com>
2024-05-30 15:51:45 -0300

    schema aplicacion

---
+++ src/entidades/aplicaciones/schema.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from database import BaseSchema
+
+
+class AplicacionDB(BaseSchema):
+    __tablename__ = "aplicaciones"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String, index=True)
+    descripcion = Column(String, index=True)
+    desarrollador = Column(String, index=True)
+    version = Column(String, index=True)
+    referentes = Column(String, index=True)
+    comentarios = Column(String, index=True)
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__}:{self.id} {self.nombre}>"
