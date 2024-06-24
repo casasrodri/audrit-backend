508576ca6ac71c8d9c8e1e773cdda496737d5714
Rodri Casas <cr.rodrigocasas@gmail.com>
2024-06-23 21:00:51 -0300

    id no incremental

--- src/entidades/links/schema.py
+++ src/entidades/links/schema.py
@@ -5,7 +5,7 @@ from database import BaseSchema
 class LinkDB(BaseSchema):
     __tablename__ = "links"
 
-    id = Column(String, primary_key=True, autoincrement=True)
+    id = Column(String, primary_key=True, index=True)
     ent1 = Column(String, nullable=False)
     id1 = Column(Integer, nullable=False)
     ent2 = Column(String, nullable=False)
